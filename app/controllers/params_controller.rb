57750aba957a4038648d164f6892b089fd7c7e96
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-10 20:03:32 -0800

    update params

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,28 +1,43 @@
 class ParamsController < ApplicationController
 
   def params_home
-    @name_array = []
-    @name_array << params[:name].upcase
-
-    @name_array.each do |name|
-      if name.start_with?("A")
-        @a_name = "Hey #{@a_name}, your name starts with the first letter of the alphabet!"
+    @name = params[:name].upcase
+      if @name.start_with?("A")
+        @a_name = "Hey, your name starts with the first letter of the alphabet!"
       end
+  end
 
+
+
+  def guess_a_num
+    winning_number = 32
+    chosen_number= params[:number].to_i
+    if chosen_number > winning_number
+      @message = "Guess lower"
+    elsif chosen_number < winning_number
+      @message = "Guess higher"
+    elsif chosen_number == winning_number
+      @message = "Correct!"
     end
   end
 
-  def guess_a_num
-    @message = "Guess a number between 1 & 100."
-    num = gets.chomp.to_s
 
-    if num == params[:num]
-      @winning_message = "Correct!"
-    elsif num > params[:num]
-      @too_high_message = "Guess lower!"
-    elsif num < params[:num]
-      @too_low_message = "Guess higher!"
+  def guess_a_num_v2
+    winning_number = 27
+    chosen_number= params[:number].to_i
+    if chosen_number > winning_number
+      @message = "Guess lower"
+    elsif chosen_number < winning_number
+      @message = "Guess higher"
+    elsif chosen_number == winning_number
+      @message = "Correct!"
     end
+  end
+
+
+  def url_params
+    @message = params[:message]
+    @second_message = params[:second_message]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/params_home", to: "params#params_home"
   get "/guess_a_num", to: "params#guess_a_num"
+  get "/url_parameter/:message/:second_message", to: "params#url_params"
+  get "/url_params/:number/", to: "params#guess_a_num_v2"
 end
